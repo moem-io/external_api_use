9abd1f55e854d2130e85de67c62826cceb4736be
lawiparadise <lawiparadise@gmail.com>
2017-04-27 23:57:46 +0900

    first

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+weather_servicekey = os.environ['weather_servicekey']
+id_db = os.environ['id_db']
+ps_db = os.environ['ps_db']
+host_db = os.environ['host_db']
+name_db = os.environ['name_db']
